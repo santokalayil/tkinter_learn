2071dea54f80a6035e36c27e416bcdd395ed27d5
Santo K Thomas <49450970+santokalayil@users.noreply.github.com>
2021-07-02 14:03:20 +0530

    committing files

---
+++ settings/locations.py
@@ -0,0 +1,5 @@
+import os
+assets_folder = 'assets'
+icon_folder = os.path.join(assets_folder, 'icons')
+menu_icon_folder = os.path.join(icon_folder, 'menu')
+eccl_folder = os.path.join(assets_folder, 'ecclesiastica')
