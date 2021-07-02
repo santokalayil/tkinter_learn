b54087062a20f81b43c43c9b8645b3cb534eb866
Santo K Thomas <49450970+santokalayil@users.noreply.github.com>
2021-07-02 14:03:19 +0530

    committing files

---
+++ gui/title_section.py
@@ -0,0 +1,10 @@
+# from tkinter import Label, RIGHT, PhotoImage
+# from settings.locations import eccl_folder
+#
+# label_image = PhotoImage(file=os.path.join(eccl_folder, 'ecl_long.png'))
+# label = Label(window, text="Hello World",
+#               font=('Arial', 40, 'bold'),
+#               fg='green', image=label_image, compound='bottom')
+# # label.pack()
+# label.pack()
+
